5ae82f73df77b6265a7ba36430a716f1cacf87ac
guiAlberto <g.alberto.moraes@gmail.com>
2016-12-05 12:29:47 -0200

    Construção de classes e assinaturas
    Aqui as principais regras são aplicadas através das classes e
    assinaturas dos métodos. Perceba que a implementação dos métodos ainda
    não foi implementada, mas as assinaturas sim.

---
+++ junit/src/main/java/unibh/es/junit/BinaryTree.java
@@ -0,0 +1,83 @@
+package unibh.es.junit;
+
+/**
+ * The BinaryTree class wraps information about the root node of a tree.
+ */
+public class BinaryTree {
+
+	/**
+	 * Add a value to the tree.
+	 * 
+	 * @param value
+	 *            Integer value to be inserted into the tree.
+	 */
+	public void add(Integer value) {
+
+	}
+
+	/**
+	 * Returns the number of nodes into the tree.
+	 * 
+	 * @return the number of nodes into the tree.
+	 */
+	public Integer size() {
+		return null;
+	}
+
+	/**
+	 * Returns the root node of the tree.
+	 * 
+	 * @return the root node of the tree.
+	 */
+	public Node getRoot() {
+		return null;
+	}
+
+	/**
+	 * Returns the height of the tree.
+	 * 
+	 * @return the height of the tree.
+	 */
+	public Integer getHeight() {
+		return null;
+	}
+
+	/**
+	 * Returns true if the tree is empty.
+	 * 
+	 * @return true if the tree is empty.
+	 */
+	public Boolean isEmpty() {
+		return null;
+	}
+
+	/**
+	 * Removes a value from the tree.
+	 * 
+	 * @param value
+	 *            Integer value to be removed from the tree.
+	 * @throws IllegalArgumentException
+	 *             in case of the tree does not contains the value.
+	 */
+	public void remove(Integer value) throws IllegalArgumentException {
+	}
+
+	/**
+	 * Returns the minimul value from the tree.
+	 * 
+	 * @return the minimum value from the tree.
+	 */
+	public Integer getMinValue() throws Exception {
+		return null;
+	}
+
+	/**
+	 * Returns the maximum value from the tree.
+	 * 
+	 * @return the maximum value from the tree.
+	 */
+	public Integer getMaxValue() throws Exception {
+		return null;
+	}
+
+}
---
+++ junit/src/main/java/unibh/es/junit/Node.java
@@ -0,0 +1,19 @@
+package unibh.es.junit;
+
+/**
+ * The Node class wraps the Integer value of a node, as well as its right and
+ * left children nodes.
+ *
+ */
+public class Node {
+
+	/**
+	 * Returns true if the node does not contains child.
+	 * 
+	 * @return true if the node does not contains child.
+	 */
+	public Boolean isLeaf() {
+		return null;
+	}
+
+}
